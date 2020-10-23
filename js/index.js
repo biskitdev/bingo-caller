696d201077cc296d707018f8fc54c32474c6d18f
biskitdev <64042275+myxxxlabs@users.noreply.github.com>
2020-10-24 10:52:50 +1300

    Add working tts and bingo calling functionality

--- js/index.js
+++ js/index.js
@@ -2,9 +2,24 @@ var rows = ["A", "B", "C", "D", "E"];
 var columns = ["1", "2", "3", "4", "5"];
 
 var otpt = document.getElementById("otpt");
+var tts = document.getElementById("tts");
 
 function generate()
 {
-    let row = rows[Math.floor(Math.random()) * 5];
-    otpt.innerHTML = row;
+    var row = rows[Math.floor(Math.random() * 5)];
+    var column = columns[Math.floor(Math.random() * 5)];
+
+    otpt.innerHTML = row + column;
+
+    if(row == "A")
+    {
+        row = "ae"
+    }
+
+    var voice = window.speechSynthesis.getVoices()[1];
+
+    var msg = new SpeechSynthesisUtterance();
+    msg.voice = voice;
+    msg.text = `${row}${column}`;
+    window.speechSynthesis.speak(msg);
 }
